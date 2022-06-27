c2507e331e127507c591846ff374ec6becd5818f
open-dataset <108185991+open-dataset@users.noreply.github.com>
2022-06-27 18:23:43 +0300

    Update postprocess.js

--- postprocess.js
+++ postprocess.js
@@ -1,6 +1,6 @@
 // Helper library written for useful postprocessing tasks with Flat Data
 // Has helper functions for manipulating csv, txt, json, excel, zip, and image files
-import { readJSON, writeJSON, removeFile } from 'https://deno.land/x/flat@0.0.14/mod.ts' 
+import { readJSON, writeJSON, removeFile } from './flat/mod.ts' 
 try {
   // Step 1: Read the downloaded_filename JSON
   const filename = Deno.args[0] 
