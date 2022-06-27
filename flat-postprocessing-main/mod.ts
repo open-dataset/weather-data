75bf4ffa65a5cc1fde266e8a36bd3ca8aa5a6af4
open-dataset <108185991+open-dataset@users.noreply.github.com>
2022-06-27 18:21:59 +0300

    Delete flat-postprocessing-main directory

--- flat-postprocessing-main/mod.ts
+++
@@ -1,7 +0,0 @@
-export * from './src/json.ts'
-export * from './src/csv.ts'
-export * from './src/image.ts'
-export * from './src/xlsx.ts'
-export * from './src/zip.ts'
-export * from './src/remove.ts'
-export * from './src/txt.ts'
